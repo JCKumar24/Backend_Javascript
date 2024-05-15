c2316d259b577fdf3d377776d366b797ea63c683
JCKumar24 <jckumar@outlook.in>
2024-05-15 19:25:09 +0530

    Server Setting

---
+++ src/index.js
@@ -0,0 +1,34 @@
+// require("dotenv").config({ path: "../.env" });
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+
+dotenv.config({ path: "../.env" });
+
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server running on port ${process.env.PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log("MONGO DB Connection error: ", error);
+  });
+
+// import express from "express";
+// const app = express();
+
+// (async () => {
+//   try {
+//     await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+//     app.on("error", (error) => {
+//       console.log("ERROR: ", error);
+//       throw error;
+//     });
+//     app.listen(process.env.PORT, () => {
+//       console.log(`Server running on port ${process.env.PORT}`);
+//     });
+//   } catch (error) {
+//     console.log("ERROR: ", error);
+//     throw error;
+//   }
+// })();
