9f0dd0e722a561f4cb508617a8915abee8b6bcb0
JCKumar24 <jckumar@outlook.in>
2024-05-16 06:35:55 +0530

    utils codes

---
+++ src/utils/ApiError.js
@@ -0,0 +1,24 @@
+class ApiError extends Error {
+  constructor(
+    status,
+    message = "Something went wrong",
+    errors = [],
+    statck = ""
+  ) {
+    super(message);
+    this.status = status;
+    this.message = message;
+    this.data = null;
+    this.message = message;
+    this.success = false;
+    this.errors = errors;
+
+    if (statck) {
+      this.stack = statck;
+    } else {
+      Error.captureStackTrace(this, this.constructor);
+    }
+  }
+}
+
+export { ApiError };
---
+++ src/utils/ApiResponse.js
@@ -0,0 +1,7 @@
+class ApiResponse {
+  constructor(status, message, data) {
+    this.status = status;
+    this.message = message;
+    this.data = data;
+  }
+}
