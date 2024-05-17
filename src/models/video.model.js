96445c8c8494c67de6e0fe4fa5d10af6ba26aa20
JCKumar24 <jckumar@outlook.in>
2024-05-17 17:05:13 +0530

    add user and video model

---
+++ src/models/video.model.js
@@ -0,0 +1,42 @@
+import mongoose, { Schema } from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+
+const videoSchema = new Schema(
+  {
+    videoFile: {
+      type: String, //cloudnary url
+      required: true,
+    },
+    thimbnail: {
+      type: String, //cloudnary url
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    duration: {
+      type: number,
+      default: 0,
+    },
+    views: {
+      type: number,
+      default: 0,
+    },
+    isPublished: {
+      type: Boolean,
+      default: true,
+    },
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  { timestamps: true }
+);
+videoSchema.plugin(mongooseAggregatePaginate);
+export const Video = mongoose.model("Video", videoSchema);
